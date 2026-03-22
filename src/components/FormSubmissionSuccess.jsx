84dd7c29e37f031bfab800743673dbf9439c2a18
SaharBatol <sahar.batol@yahoo.co.uk>
2026-03-22 14:11:07 +0000

    created form submission success component

---
+++ src/components/FormSubmissionSuccess.jsx
@@ -0,0 +1,13 @@
+const FormSubmissionSuccess = () => {
+  return (
+    <>
+      <h1 className="heading">Thank you for your feedback!</h1>
+      <h2 className="subheading">
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don't hesitate to get in touch!
+      </h2>
+    </>
+  );
+};
+
+export default FormSubmissionSuccess;
